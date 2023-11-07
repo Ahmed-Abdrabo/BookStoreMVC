d09d3913640405ad5d3117d400a1713987267f07
AhmedAbdrabo <am3991156@gmail.com>
2023-11-07 09:17:31 +0200

    Add_category_CRUD

---
+++ BookStore/Controllers/CategoryController.cs
@@ -0,0 +1,90 @@
+﻿using BookStore.Data;
+using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> catList=_db.Categories.ToList();
+            return View(catList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category cat)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(cat);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cat);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var category = _db.Categories.FirstOrDefault(c => c.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category cat)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(cat);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cat);
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var category = _db.Categories.FirstOrDefault(c => c.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost,ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            var category = _db.Categories.FirstOrDefault(c => c.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            
+              _db.Categories.Remove(category);
+              _db.SaveChanges();
+              return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- BookStore/Program.cs
+++ BookStore/Program.cs
@@ -1,7 +1,14 @@
+using BookStore.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Register ApplicationDbContext as a dependency
+builder.Services.AddDbContext<ApplicationDbContext>(options=>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
 var app = builder.Build();
 
